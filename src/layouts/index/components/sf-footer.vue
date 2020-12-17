b3e59fb79da660f9bdd996b8b3eebda1047676d7
zhangzilong <315737455@qq.com>
2020-12-17 16:25:29 +0800

    新增footer

--- src/layouts/index/components/sf-footer.vue
+++ src/layouts/index/components/sf-footer.vue
@@ -1,5 +1,17 @@
 <template>
-  <v-footer app> </v-footer>
+  <v-footer app>
+    <v-col class="text-center" cols="12">
+      {{ new Date().getFullYear() }}
+      -
+      <a href="https://github.com/BreadMage" target="_blank">
+        SaltyFish
+      </a>
+      -
+      <a href="https://github.com/BreadMage/sf-admin-vuetify" target="_blank">
+        Github
+      </a>
+    </v-col>
+  </v-footer>
 </template>
 
 <script>
